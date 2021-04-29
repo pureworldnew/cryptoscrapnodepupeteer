23041f2a53aa24b043f5c39049fc7f682f0b0109
alexey <pureworldnew@gmail.com>
2021-04-29 09:56:01 -0400

    add new test

---
+++ puppet.js
@@ -0,0 +1,16 @@
+const puppeteer = require("puppeteer");
+(async () => {
+    const browser = await puppeteer.launch({executablePath: '/usr/bin/chromium-browser',headless: true, args: ['--no-sandbox']});
+    const page = await browser.newPage();
+    await page.goto("https://pancakebunny.finance/pool", {
+      waitUntil: "networkidle2",
+    });
+    await page
+      .waitForXPath(
+        "/html/body/div/div[3]/div[1]/div[2]/div/div/div/div[2]/div[1]/div/div[3]/div/div/div/span"
+      )
+      .then(() => console.log("waited for Rewards value"));
+
+    await page.screenshot({ path: "example.png" });
+    await browser.close();
+  })();
